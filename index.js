34323b99203125c14d8b216f90b01806b3660e1a
dangwin <danghwin@gmail.com>
2020-12-01 19:15:07 -0600

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(arr){
+  let winning = arr.find(obj => {
+    return obj.result === "W"
+  })
+  return winning ? winning.year : undefined
+}
